1eb5723ebd527f478680ef4899c4a587d0dbb028
Jan <kelevra.1337@gmail.com>
2020-12-17 01:17:03 +0100

    cleanup

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const fsPromises = require('fs').promises
 const crypto = require('crypto');
 const URL = require('url');
-const glob = require('glob');
 
 const urlList = require('./config/urlList');
 
@@ -19,7 +18,6 @@ function generateChecksum(str, algorithm, encoding) {
 function timeout(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 
 const screenshot = async (url, screenshotPath) => {
   const browser = await puppeteer.launch();
@@ -41,36 +39,8 @@ if (!fs.existsSync("./screenshots/")) {
   fs.mkdirSync("./screenshots/");
 }
 
-const checksum1 = generateChecksum(file);
-
-
-console.log(checksum1); */
-/* 
-screenshot("https://luedemann2.de/",5,"1.png");
-screenshot("https://luedemann2.de/",5,"2.png");
-screenshot("https://luedemann2.de/",5,"3.png");
- */
-
-
-if (!fs.existsSync("./screenshots/")) {
-  fs.mkdirSync("./screenshots/");
-}
 
 (async () => {
-
-  const oldFiles = []
-  _.forEach(urlList, async (url) => {
-    const oldFiles = await fsPromises.readFile(url);  
-  })
-
-
-  // create new screenshots
-  await Promise.all(_.map(urlList, (url) => {
-    const filePath = "./screenshots/" + URL.parse(url).hostname + "_" + (+new Date()) + ".png";
-    return screenshot(url,3, filePath);
-  }))
-
-  //
 
   // generate all promises from the url list
   // async keyword turns return value into a promise, even if it resolves instantly
