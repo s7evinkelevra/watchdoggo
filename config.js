4f4305570ec0c9793eb5203e11ff69bdc392aacc
Jan Lüdemann <kelevra.1337@gmail.com>
2020-12-25 02:52:20 +0100

    moved url list to own file, config is now mostly in env vars

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  urlList: [
-    "https://luedemann2.de/",
-    "https://berufseinstieg-bundeswehr.de",
-  ],
-  reportEmail: "kelevra.1337@gmail.com"
-}
---
+++ urlList.js
@@ -0,0 +1,4 @@
+module.exports = [
+  "https://luedemann2.de/",
+  "https://berufseinstieg-bundeswehr.de",
+]
